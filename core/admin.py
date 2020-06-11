1049c01a43ec011fc38be9d48c17c2e56cd2dbc6
Ingrid <dingrisouzz@gmail.com>
2020-06-11 15:37:50 -0300

    Criando tabela de eventos

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from core.models import Evento
 
 # Register your models here.
+
+
+
+class EventoAdmin(admin.ModelAdmin):
+    #apresenta no bd o titulo e as datas do evento na listagem
+    list_display = ('titulo', 'data_evento', 'data_criacao')
+    #Cria um filtro para filtrar por nome de usuario, titulo e data de evento
+    list_filter = ('usuario', 'titulo', 'data_evento',)
+
+admin.site.register(Evento, EventoAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100) #cria o nome do evento
+    descricao = models.TextField(blank=True, null=True) #a descricao do evento pode ser nula ou em branco
+    data_evento = models.DateTimeField(verbose_name='Data do evento')
+    data_criacao = models.DateTimeField(auto_now=True) #insere a data atual do sistema
+    usuario = models.ForeignKey(User, on_delete= models.CASCADE) #se o dono do evento for excluido vc exclui todos os eventos dele
+
+    class Meta:
+        db_table= 'evento' #o nome da tabela é evento
+
+    def __str__(self): #apresenta no bd o titulo do evento na listagem
+        return self.titulo
